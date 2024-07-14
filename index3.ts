a798fdb9e2eb590e54b79d9d3da2ff391002bda1
wnstndks <anjs9912@gmail.com>
2024-07-15 02:01:42 +1000

    type alias

--- index3.ts
+++ index3.ts
@@ -93,3 +93,109 @@ function 함수이다3(x: number | string) {
     // 2. 무슨 타입이 들어올지 100%확실할 때 사용
     // 3. 남이 짠 코드 수정, 왜 타입에러가 나는지 모르겠을때만 비상용으로..
 }
+
+
+
+// type alias 만드는 법
+// let 동물:string|number| undefined;
+// union 타입으로 동물이 들어감
+
+type Animal = string| number|undefined;
+let 동물:Animal // 위의 타입들만 동물에 들어갈 수 있다 -> 타입키워드로 변수 만드는 것= type alias
+
+// union 타입 뿐만 아니라, object타입도 변수에 담을 수 있음
+// type 변수 작명 관습 - 일반변수와 다르게 대분자로 쓰기
+type AnimalType={name:string, age: number}
+let 동물2: AnimalType = {name:'kim',age:20}
+
+// const 변수 특징 - 절대 바꿀수 없는 변수(재할당금지)
+const 출생지역2={region:'seoul'}
+출생지역2.region='busan'
+// const변수는 등호로 재할당만 막는 역할
+// const로 담은 object 수정은 자유롭게 가능
+
+// typescript 쓰면 object 자료 수정도 막을 수 있음
+type Girlfriend={
+    // 읽기전용으로 바꿔줌- 수정 불가능 - 수정시 에러남
+    readonly name:string
+}
+
+const 여친:Girlfriend= {
+    name:'엠버'
+}
+
+
+// 여친.name='유라' 
+// 타입스크립트 에러는 에디터& 터미널에서만 존재
+// 실제 변환된 js파일은 에러없음
+
+// type Girlfriend={
+//     name?:string -> string|undefined가 들어갈수 있다는 뜻
+// }
+
+type Name=string;
+type Age= number;
+
+// type 변수 - 당연히 union type으로 합치기 가능
+type Person = Name | Age;
+
+type PositionX = {x:number};
+type PositionY = {y:number};
+
+// &연산자로 object 타입 합치기(extend하기)
+type NewType=PositionX&PositionY;
+let position:NewType={x:10,y:20}
+
+// 타입키워드는 재정의는 불가능
+
+
+type 내타입={x:number}
+type 내타입2={x:number}
+
+type NewType2=내타입&내타입2
+let position2:NewType2={x:10}
+
+
+type 내타입3={
+    color?:string,
+    size:number,
+    readonly position: number[]
+}
+
+let 테스트용변수3:내타입3={
+    size:123,
+    position:[1,2,3]
+}
+
+type 내정보={
+    name:string,
+    phone:number,
+    email?:string
+}
+
+let 테스트용변수4:내정보={
+    name:'하이',
+    phone:123456
+}
+
+type 미성년자여부={어른:boolean}
+type 뉴타입=내정보&미성년자여부
+let 테스트용변수5:뉴타입={
+    name:'예스',
+    phone:1234,
+    어른:true
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+

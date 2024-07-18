9e211e49c4e3061ba90df3586b70f85a8350ac9b
wnstndks <anjs9912@gmail.com>
2024-07-19 01:44:43 +1000

    function

--- index4.ts
+++ index4.ts
@@ -71,6 +71,26 @@ function 내함수(a:'kim'){
 내함수(자료.name)
 
 
+let 회원정보 = {
+  name : 'kim',
+  age : 30,
+  plusOne (x){
+    return x + 1
+  },
+  changeName : () => {
+    console.log('안녕')
+  }
+}
+회원정보.plusOne(1);
+회원정보.changeName();
+
+type Member={
+  name:string,
+  age:number,
+  plusOne=(x:number)=>number,
+
+
+}
 
 
 

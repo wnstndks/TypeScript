b0ef6dae2b908efb75f1185d129a07f73e717aa4
wnstndks <anjs9912@gmail.com>
2024-09-26 22:53:31 +0900

    rest parameter

--- pr2/index5.ts
+++ pr2/index5.ts
@@ -1,173 +1,173 @@
-// class Person{
-//     // constructer에서는 각각의 인스턴스들에 대한 변수들을 만들어놓아야 함
-//     // data=0; // 필드값 -> 모든 자식들은 data라는 값을 가질 수 있음 - 타입지정 가능
-//     name:string;
-//     age:number;
+// // class Person{
+// //     // constructer에서는 각각의 인스턴스들에 대한 변수들을 만들어놓아야 함
+// //     // data=0; // 필드값 -> 모든 자식들은 data라는 값을 가질 수 있음 - 타입지정 가능
+// //     name:string;
+// //     age:number;
 
-//     constructor(a:string,b:number){
-//         // return은 굳이 해줄필욘 없음 - 복제되는게 다 object이기에 그리고 rest parameter, default parameter도 가능
-//         this.name=a
-//         this.age=b
-//     }
+// //     constructor(a:string,b:number){
+// //         // return은 굳이 해줄필욘 없음 - 복제되는게 다 object이기에 그리고 rest parameter, default parameter도 가능
+// //         this.name=a
+// //         this.age=b
+// //     }
 
-//     // prototype에 함수도 만들어줄 수 있음
-//     함수(a:string){
-//         console.log('안녕'+a);
-//     }
-// }
-
-
-// // console.log(사람1.data)
-
-// let 사람1= new Person('홍길동',20);
-// let 사람2= new Person('아무개',24);
+// //     // prototype에 함수도 만들어줄 수 있음
+// //     함수(a:string){
+// //         console.log('안녕'+a);
+// //     }
+// // }
 
 
-// // class 문제1
+// // // console.log(사람1.data)
 
-// class Car{
-//     model:string;
-//     price:number;
-
-//     constructor(name,price){
-//         this.model=name;
-//         this.price=price;    
-//     }
-
-//     tax():number{
-//         return this.price/10;
-//     }
-// }
+// // let 사람1= new Person('홍길동',20);
+// // let 사람2= new Person('아무개',24);
 
 
-// let car1= new Car('소나타',3000);
-// console.log(car1)
-// console.log(car1.tax())
+// // // class 문제1
+
+// // class Car{
+// //     model:string;
+// //     price:number;
+
+// //     constructor(name,price){
+// //         this.model=name;
+// //         this.price=price;    
+// //     }
+
+// //     tax():number{
+// //         return this.price/10;
+// //     }
+// // }
 
 
-// // class 문제2
+// // let car1= new Car('소나타',3000);
+// // console.log(car1)
+// // console.log(car1.tax())
 
-// class Word{
-//     num:number[];
-//     str:string[];
 
-//     constructor(...param:(number|string)[]){
+// // // class 문제2
+
+// // class Word{
+// //     num:number[];
+// //     str:string[];
+
+// //     constructor(...param:(number|string)[]){
         
-//         let 숫자들:number[]=[];
-//         let 문자들:string[]=[];
+// //         let 숫자들:number[]=[];
+// //         let 문자들:string[]=[];
 
-//         param.forEach((a)=>{
-//             if(typeof a==='number'){
-//                 숫자들.push(a);
-//             }else{
-//                 문자들.push(a);
-//             }
-//         })
+// //         param.forEach((a)=>{
+// //             if(typeof a==='number'){
+// //                 숫자들.push(a);
+// //             }else{
+// //                 문자들.push(a);
+// //             }
+// //         })
 
-//         this.num=숫자들;
-//         this.str=문자들;
-//     }
-// }
+// //         this.num=숫자들;
+// //         this.str=문자들;
+// //     }
+// // }
 
-// let obj=new Word('kim',3,5,'park');
+// // let obj=new Word('kim',3,5,'park');
 
-// console.log(obj.num);
-// console.log(obj.str);
+// // console.log(obj.num);
+// // console.log(obj.str);
 
 
-// type 키워드로 타입변수 생성가능 및 interface 키워드로도 타입변수 생성가능
+// // type 키워드로 타입변수 생성가능 및 interface 키워드로도 타입변수 생성가능
 
-// type 네모타입={color:string,width:number};
-// interface 네모타입2{
-//     color:string,
-//     width:number
-// }; // 일종의 class 만드는 법과 유사함 - 차이는??
+// // type 네모타입={color:string,width:number};
+// // interface 네모타입2{
+// //     color:string,
+// //     width:number
+// // }; // 일종의 class 만드는 법과 유사함 - 차이는??
 
-// let 네모:네모타입 = {color:'red',width:100}
+// // let 네모:네모타입 = {color:'red',width:100}
 
+
+// // interface Student{
+// //     name:string;
+// // }
+
+// // interface Teacher{
+// //     name:string;
+// //     age:number;
+// // }
+
+// // let 학생:Student= {name:'kim'};
+// // let 선생:Teacher= {name:'kim',age:20};
+
+// // 인터페이스를 쓰면 extends로 복사가능
+// // Student와 Teacher이 중복됨 -> Student에 있던걸 복사해서 Teacher에 쓰기
 
 // interface Student{
 //     name:string;
 // }
 
-// interface Teacher{
-//     name:string;
+// interface Teacher extends Student{
 //     age:number;
 // }
 
-// let 학생:Student= {name:'kim'};
-// let 선생:Teacher= {name:'kim',age:20};
-
-// 인터페이스를 쓰면 extends로 복사가능
-// Student와 Teacher이 중복됨 -> Student에 있던걸 복사해서 Teacher에 쓰기
-
-interface Student{
-    name:string;
-}
-
-interface Teacher extends Student{
-    age:number;
-}
-
-type Animal={name:string}
-type Cat={age:number} & Animal //intersection type을 써서 타입을 추가해주기 - 두 타입을 전부 만족하는 타입
-// interface도 &기호를 써서 교차타입을 만들수 있음
-let 선생2:Cat={name:'kim',age:20}
+// type Animal={name:string}
+// type Cat={age:number} & Animal //intersection type을 써서 타입을 추가해주기 - 두 타입을 전부 만족하는 타입
+// // interface도 &기호를 써서 교차타입을 만들수 있음
+// let 선생2:Cat={name:'kim',age:20}
 
 
-// interface와 type
-// interface는 중복선언 가능 -자동 익스텐드가됨
-// type은 중복선언 불가능
+// // interface와 type
+// // interface는 중복선언 가능 -자동 익스텐드가됨
+// // type은 중복선언 불가능
 
-interface Student{
-    name:string
-}
-interface Student{
-    score:number
-}
+// interface Student{
+//     name:string
+// }
+// interface Student{
+//     score:number
+// }
 
-// 외부 라이브러리의 경우 interface 를 많이 사용
-//  다른 사람이 이용많이 할것 같으면 object 타입 지정할 때 interface 사용
-// extends쓸때 중복 속성 발생시 에러
-// &기호쓸때 중복 속성 발생시 미리 에러가 나지 않음
+// // 외부 라이브러리의 경우 interface 를 많이 사용
+// //  다른 사람이 이용많이 할것 같으면 object 타입 지정할 때 interface 사용
+// // extends쓸때 중복 속성 발생시 에러
+// // &기호쓸때 중복 속성 발생시 미리 에러가 나지 않음
 
-// interface 문제1
-interface Product{
-    brand:string;
-    serialNumber:number;
-    model:string[]
-}
+// // interface 문제1
+// interface Product{
+//     brand:string;
+//     serialNumber:number;
+//     model:string[]
+// }
 
-let 상품:Product={brand:'Samsung',serialNumber:1360,model:['TV','Phone']}
+// let 상품:Product={brand:'Samsung',serialNumber:1360,model:['TV','Phone']}
 
-// 문제2
-interface Product2{
-    product:string;
-    price:number;
-}
+// // 문제2
+// interface Product2{
+//     product:string;
+//     price:number;
+// }
 
-// let 장바구니:(Product2:Product3)[]=[{product:'청소기',price:7000},{product:'삼다수',price:800}]
+// // let 장바구니:(Product2:Product3)[]=[{product:'청소기',price:7000},{product:'삼다수',price:800}]
 
-// 문제3
-interface Product3 extends Product2{
-    card:boolean
-}
+// // 문제3
+// interface Product3 extends Product2{
+//     card:boolean
+// }
 
-let 장바구니:(Product2|Product3)[]=[{product:'청소기',price:7000},{product:'삼다수',price:800},{product:'청소기',price:80000,card:false}]
+// let 장바구니:(Product2|Product3)[]=[{product:'청소기',price:7000},{product:'삼다수',price:800},{product:'청소기',price:80000,card:false}]
 
-// 문제4
+// // 문제4
 
-interface PlusType{
-    plus:(x:number , y:number)=>number;
-    minus:(x:number , y:number)=>number;
-}
+// interface PlusType{
+//     plus:(x:number , y:number)=>number;
+//     minus:(x:number , y:number)=>number;
+// }
 
-let 오브젝트:PlusType={
+// let 오브젝트:PlusType={
 
-    plus(a,b){
-        return a+b
-    },
-    minus(a,b){
-        return a-b
-    }
-}
+//     plus(a,b){
+//         return a+b
+//     },
+//     minus(a,b){
+//         return a-b
+//     }
+// }
---
+++ pr2/index6.ts
@@ -0,0 +1,64 @@
+// rest parameter
+// 몇개의 파라미터가 들어올 지 모를때 ... 을 사용함, 다른 파라미터 있으면 맨 뒤에만 사용가능하다
+// function 함수(...a){
+//     console.log(a)
+// }
+
+// 함수(1,2,3,4,5,6) // 파라미터로 들어온 걸 다 array로 출력하고 있음 rest parameter는 array 형식
+
+// rest parameter 타입지정
+// function 함수(...a: number[]){
+//     console.log(a)
+// }
+
+// 함수(1,2,3,4,5,6)
+
+// ... 세개의 첫번째 용도 - 함수 안에 쓰면 -rest parameter 만약 array나 object에쓰면 괄호 벗기기
+// let arr =[1,2,3];
+// let arr2 = [4,5];
+// let arr3 = [...arr,...arr2]; // 이 오브젝트 안에 있는 괄호를 벗겨달라는 뜻
+
+
+// destructuring 개념설명
+// let arr= ['안녕',100] // 이 자료들을 변수로 빼서 쓰고 싶으면
+// let [변수1, 변수2] = ['안녕',100] // 왼쪽 오른쪽 똑같이 자료를 맞추면 변수가 나옴
+// console.log(변수1)
+
+// let {student:student, age:age} =  {student:true,age:20}; 
+// let {student,age}={student:true,age:20};// 이름이 같으면 생략해도 됨
+// let 오브젝트 = {student:true,age:20};
+
+// function 함수(a,b){
+//     console.log(a,b)
+// }
+
+// 함수(오브젝트.student,오브젝트.age);
+
+// function 함수({student,age}:{student:boolean,age:number}){
+//     console.log(student,age)
+// }
+
+// 함수({student:true,age:20}) //파라미터 만들기 == 변수만들기
+
+// rest parameter 문제1
+// function 최댓값함수(...a:number[]){
+//     let max:number=0;
+//     a.forEach((i)=>{
+//         if(max<i){
+//             max=i;
+//         }
+//     })
+    
+//     return max;
+// }
+
+// rest parameter 문제2
+// function 함수({user, comment, admin}:{user:number,comment:number[],admin:boolean}){
+//     console.log(user, comment, admin)
+
+// }
+
+// 문제3
+// function 마이함수([...a]:(number|string|boolean)[]){
+//     console.log(a)   
+// }

d8ad52e8203013a48e74ecd20953588f81029e0a
junsu <anjs9912@gmail.com>
2024-10-03 00:28:42 +0900

    redux typescript로 수정하는법

--- reactapp/src/index.tsx
+++ reactapp/src/index.tsx
@@ -1,19 +1,37 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { createSlice, configureStore, PayloadAction } from '@reduxjs/toolkit';
+import { Provider } from 'react-redux';
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// (신규방식 redux) state와 reducer 만들 때 타입지정 필요  
+// 신식 redux를 사용하고 싶으면 redux, react-redux에 이어서 
+// npm install @reduxjs/toolkit
+
+const 초기값 = { count: 0, user : 'kim' };
+
+const counterSlice = createSlice({
+  name: 'counter',
+  initialState : 초기값,
+  reducers: {
+    증가 (state){
+      state.count += 1
+    },
+    감소 (state){
+      state.count -= 1
+    },
+    맘대로증가 (state, action :PayloadAction<number>){
+      state.count += action.payload
+    }
+  }
+})
+
+let store = configureStore({
+  reducer: {
+    counter1 : counterSlice.reducer
+  }
+})
+
+//state 타입을 export 해두는건데 나중에 쓸 데가 있음
+export type RootState = ReturnType<typeof store.getState>
+
+//수정방법 만든거 export
+export let {증가, 감소, 맘대로증가} = counterSlice.actions
